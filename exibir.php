4cfd990e5ab33f855831eb1fba4a1a7f0dd45a7b
AdsonCoelho <adson.coelho@gmail.com>
2022-08-01 22:59:50 -0400

    Ajustes detalhar imagens da página exibir.php

--- exibir.php
+++ exibir.php
@@ -7,9 +7,9 @@ $db = "db_aej";
 
 $con = mysqlI_connect($host,$username,$password) or die("Impossível conectar ao banco.");
 
- mysqlI_select_db($con,$db) or die("Impossível conectar ao banco");
+mysqlI_select_db($con,$db) or die("Impossível conectar ao banco");
 
- $sql = "SELECT * FROM PRODUTOS";
+$sql = "SELECT * FROM PRODUTOS";
 
 $result=mysqlI_query($con,$sql) or die("Impossível executar a query");
 
@@ -33,21 +33,21 @@ $result=mysqlI_query($con,$sql) or die("Impossível executar a query");
 		</nav>
 	
 		<main class="conteudo">
-		<section class="">
-			<h1>Exibindo os produtos cadastrados </h1>
-			<?php
-				while($row = mysqlI_fetch_object($result)) {
-					echo "<br> <img src='getImagem.php?PicNum=$row->PROD_ID' width=150 height=200 border=1'/><br>";
-					echo $row->PROD_DESC."<br><a href='getImagem.php?PicNum=$row->PROD_ID'>Exibir detalhes</a><br>";				
-					}									
-					
+			<section class="">
+				<h1>Exibindo os produtos cadastrados </h1>
+				<?php
+					while($row = mysqlI_fetch_object($result)) {
+						echo "<img src='getImagem.php?PicNum=$row->PROD_ID'width=250 height=350 border=1 >";
+						echo "<a href='getImagem.php?PicNum=$row->PROD_ID'>Exibir detalhes</a>";				
+
+					}		
 					mysqli_close($con);
 				?>
-		</section>
-	</main>
+			</section>
+		</main>
         
-	<footer class="rodape">
-		<img class="img_rodape" src="imagens/rodape.png"></a>
-	</footer>  
+		<footer class="rodape">
+			<img class="img_rodape" src="imagens/rodape.png"></a>
+		</footer>  
     </body>
 </html>
